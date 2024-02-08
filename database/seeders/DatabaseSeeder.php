dc25e8de005a96a9c7fef35124cfb2bc45df6d10
iker-dp <iker.diaz@insbaixcamp.cat>
2024-02-08 22:42:12 +0100

    DatabaseSeeder actualitzat

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            //BodyPartSeeder::class,
-            //EquipmentSeeder::class,
-            //TargetSeeder::class,
-            ExerciseSeeder::class
+            BodyPartSeeder::class,
+            EquipmentSeeder::class,
+            TargetSeeder::class,
+            //ExerciseSeeder::class
         ]);
     }
 }
