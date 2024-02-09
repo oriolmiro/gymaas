31f22a74b0d1299fe804d1e94e78d6cf5268f899
AdriaChillon <144825302+AdriaChillon@users.noreply.github.com>
2024-02-09 19:30:10 +0100

    LanguageSeeder amb 4 registres

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class LanguageSeeder extends Seeder
 {
@@ -12,6 +14,24 @@ class LanguageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('languages')->insert([
+            'iso_code' => Str::of('ES'),
+            'name' => Str::of('Castellano'),
+        ]);
+
+        DB::table('languages')->insert([
+            'iso_code' => Str::of('CA'),
+            'name' => Str::of('Catalán'),
+        ]);
+
+        DB::table('languages')->insert([
+            'iso_code' => Str::of('EN'),
+            'name' => Str::of('Inglés'),
+        ]);
+
+        DB::table('languages')->insert([
+            'iso_code' => Str::of('IT'),
+            'name' => Str::of('Italiano'),
+        ]);
     }
 }
