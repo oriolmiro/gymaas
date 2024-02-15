6f7986c724e65c90024e1f652b268362037ca122
miguel.martinez <miguel.martinez35j@gmail.com>
2024-02-15 20:04:13 +0100

    .env.example configurado correctamente, enlaces dashboard aplicados

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,12 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('internacionalization.index')">
+                        {{ __('Internacionalization') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('core.index')">
+                        {{ __('Core') }}
+                    </x-nav-link>
                 </div>
             </div>
 
