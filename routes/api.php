faf2356be77e28626173f341648485323331891d
cnievaslozano <cnievaslozano@gmail.com>
2024-02-18 16:09:29 +0100

    routes bodypartlist bodypart

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BodyPartController;
+use App\Http\Controllers\ExerciseByBodyPartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+
+Route::get('/exercises/bodyPartList', BodyPartController::class);
+Route::post('/exercises/bodyPart/{bodyPart}', ExerciseByBodyPartController::class);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
