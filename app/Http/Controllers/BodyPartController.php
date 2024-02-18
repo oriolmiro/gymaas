dc43dc0c9e1008f9d6d100e2d98ca885d638794d
cnievaslozano <cnievaslozano@gmail.com>
2024-02-18 16:08:41 +0100

    api all body part

--- app/Http/Controllers/BodyPartController.php
+++ app/Http/Controllers/BodyPartController.php
@@ -7,61 +7,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreBody_partRequest;
 use App\Http\Requests\UpdateBody_partRequest;
 
+
 class BodyPartController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function __invoke()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreBody_partRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Body_part $body_part)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Body_part $body_part)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateBody_partRequest $request, Body_part $body_part)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Body_part $body_part)
-    {
-        //
+        $bodyParts = Body_part::all();
+        return response()->json($bodyParts);
     }
 }
