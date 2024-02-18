9f99cce16c7790aad37d18ddcfae303855ec3016
cnievaslozano <cnievaslozano@gmail.com>
2024-02-18 16:26:37 +0100

    Swagger docu

--- app/Http/Controllers/BodyPartController.php
+++ app/Http/Controllers/BodyPartController.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use App\Models\Body_part;
@@ -7,9 +8,27 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreBody_partRequest;
 use App\Http\Requests\UpdateBody_partRequest;
 
-
+/**
+ * @OA\Tag(
+ *     name="Exercises",
+ *     description="Endpoints relacionados con ejercicios"
+ * )
+ */
 class BodyPartController extends Controller
 {
+    /**
+     * @OA\Get(
+     *      path="/exercises",
+     *      operationId="getAllExercises",
+     *      tags={"Exercises"},
+     *      summary="Obtener todos los ejercicios",
+     *      description="Obtiene una lista de todos los ejercicios disponibles.",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Lista de ejercicios"
+     *       )
+     *     )
+     */
     public function __invoke()
     {
         $bodyParts = Body_part::all();
