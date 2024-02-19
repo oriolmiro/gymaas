54664ec8248d2892cc9838c85abfa2b11c29e36c
iker-dp <iker.diaz@insbaixcamp.cat>
2024-02-19 20:53:35 +0100

    ExerciseSeeder finalitzat

--- database/seeders/ExerciseSeeder.php
+++ database/seeders/ExerciseSeeder.php
@@ -18,36 +18,35 @@ class ExerciseSeeder extends Seeder
     public function run(): void
     {
         $client = new \GuzzleHttp\Client();
-        
-        for ($i = 0; $i < 300; $i+50) {
-            $response = $client->request('GET', 'https://exercisedb.p.rapidapi.com/exercises?limit=50&offset='.$i, [
-                'headers' => [
-                    'X-RapidAPI-Host' => 'exercisedb.p.rapidapi.com',
-                    'X-RapidAPI-Key' => $_ENV["API_KEY"],
-                ],
+        $response = $client->request('GET', 'https://exercisedb.p.rapidapi.com/exercises?limit=1400', [
+            'headers' => [
+                'X-RapidAPI-Host' => 'exercisedb.p.rapidapi.com',
+                'X-RapidAPI-Key' => $_ENV["API_KEY"],
+            ],
+        ]);
+    
+        $exercises = json_decode($response->getBody(), true);
+            
+        // Guarda tots els exercisis a la BD
+        foreach ($exercises as $exercise) {
+            $exercise_db = Exercise::create([
+                'name' => $exercise['name'],
+                'gif' => '',
+                'secondary_muscles' => implode(',', $exercise['secondaryMuscles']),
+                'instructions' => json_encode($exercise['instructions']),
+                'lang' => 'en',
+                'body_part_id' => Body_part::where('name',$exercise['bodyPart'])->first()->id,
+                'equipment_id' => Equipment::where('name',$exercise['equipment'])->first()->id,
+                'target_id' => Target::where('name',$exercise['target'])->first()->id,
+                'exercise_id' => 0
             ]);
     
-            $exercises = json_decode($response->getBody(), true);
-            
-            // Guarda tots els exercisis a la BD
-            foreach ($exercises as $exercise) {
-                $exercise_db = Exercise::create([
-                    'name' => $exercise['name'],
-                    'gif' => '',
-                    'secondary_muscles' => implode(',', $exercise['secondaryMuscles']),
-                    'instructions' => json_encode($exercise['instructions']),
-                    'lang' => 'en',
-                    'body_part_id' => Body_part::where('name',$exercise['bodyPart'])->first()->id,
-                    'equipment_id' => Equipment::where('name',$exercise['equipment'])->first()->id,
-                    'target_id' => Target::where('name',$exercise['target'])->first()->id,
-                    'exercise_id' => 0
-                ]);
-    
-                Storage::disk('local')->put($exercise_db->id.'.gif', file_get_contents($exercise['gifUrl']));
-                $exercise_db->gif = $exercise_db->id.'.gif';
-                $exercise_db->exercise_id = $exercise_db->id;
-                $exercise_db->save();
-            }
+            sleep(rand(1, 3));
+            Storage::disk('local')->put($exercise_db->id.'.gif', file_get_contents($exercise['gifUrl']));
+            $exercise_db->gif = $exercise_db->id.'.gif';
+            $exercise_db->exercise_id = $exercise_db->id;
+            $exercise_db->save();
+            print("Exercici $exercise_db->id creat\n");
         }
     }
 }
