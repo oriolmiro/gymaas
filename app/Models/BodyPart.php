2a6a519e23a3dcf7b1fde4c6eeb5ac1fe6c3ef82
Oriol <oriolmiro@gmail.com>
2024-02-19 20:33:48 +0100

    Relacions de Exercicis

--- app/Models/BodyPart.php
+++ app/Models/BodyPart.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Body_part extends Model
+class BodyPart extends Model
 {
     use HasFactory;
 
