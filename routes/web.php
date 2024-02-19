bdc665cdfb84cd994bcea891736161a9a00f36e8
y4assin <yassineelbakali30@gmail.com>
2024-02-19 20:23:36 +0100

    Se añade propiedad Deepl API KEY Al fichero .env

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Translators\DeeplLApiTranslator;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+
+});
+Route::get('/DeepLtranslate', function (){
+    $translator = new DeeplLApiTranslator();
+    echo $translator->translate('Hello, world!', 'en', 'es');
 });
