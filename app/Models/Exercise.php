06131189763f50354dda315f79fc51c0baf33c19
Cristian Manuel Alcobendas Beorlegui <cristian.alcobendas@insbaixcamp.cat>
2024-02-24 13:57:06 +0100

    Add method to get exercises pending translations

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\BodyPart;
 use App\Models\Equipment;
 use App\Models\Target;
+use Illuminate\Support\Facades\DB;
 
 class Exercise extends Model
 {
@@ -23,6 +24,18 @@ class Exercise extends Model
         'equipment_id',
         'target_id',
         'exercise_id'
+    ];
+
+    protected $columnsToTranslate = [
+        'name' => [
+            'type' => 'string',
+        ],
+        'secondary_muscles' => [
+            'type' => 'string',
+        ], 
+        'instructions' => [
+            'type' => 'json',
+        ],
     ];
 
     /**
@@ -44,5 +57,48 @@ class Exercise extends Model
     */
     public function target(){
         return $this->belongsTo(Target::class);
+    }
+
+    /** 
+     * Return an array with pending exercises translations.
+     * 
+     * @param int $limit - Default 5 pending translations.
+     * 
+     * @return Array
+     * */
+    function getExercisesPendingTranslations(int $limit = 5) {
+        // Check limit is not lower/equals than 0
+        $limit = ($limit <= 0) ? 5 : $limit;
+
+        // Generate WHERE conditions
+        $where = '';
+        $i = 0;
+
+        foreach ($this->columnsToTranslate as $columnToTranslate => $attributes) {
+            $prefix = ($i > 0) ? ' OR ' : '';
+
+            // Check column type
+            switch($attributes['type']) {
+                case 'json':
+                    $where .= $prefix . $columnToTranslate . " = '[]'";
+                    break;
+
+                case 'string':
+                    $where .= $prefix . $columnToTranslate . " = ''";
+                    break;
+            }
+
+            // Increment $i + 1
+            $i++;
+        }
+
+        // Complete and execute query
+        $query = 
+            "SELECT * "
+            . "FROM " . $this->table . " "
+            . "WHERE " . $where . " " 
+            . "LIMIT " . $limit . " ";
+        
+        return DB::select($query);
     }
 }
