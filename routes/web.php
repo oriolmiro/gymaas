7b5e8acd01d5d481e71691fe1590ad324e982530
Cosmin Calin <cosmin.calin22@gmail.com>
2024-05-02 19:22:43 +0200

    Login y vista de controlar objetos gym con API interna

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Translators\DeeplLApiTranslator;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-
-
-});
-Route::get('/DeepLtranslate', function (){
-    $translator = new DeeplLApiTranslator();
-    echo $translator->translate('Hello, world!', 'en', 'es');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('posts', PostController::class);
+});
+
+require __DIR__.'/auth.php';
